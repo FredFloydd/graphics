7a600ac39e0c2f3541b879671f94b721cc0df52f
Freddie Floydd <ftf23@cam.ac.uk>
2023-06-19 01:45:21 +0100

    change opengl api from sdl to glfw

---
+++ basics_glfw/01_window/window.cpp
@@ -0,0 +1,59 @@
+#include "../../include/glad/glad.h"
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main (int argc, char *argv[]) {
+	// Set up OpenGL options
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	// Create window object, exiting if unsuccessful
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My First Window", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	// Make created window the main context and set up viewport
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	glViewport(0, 0, 800, 600);
+
+	// Main render loop
+	while (!glfwWindowShouldClose(window)) {
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Clean up and exit after window is closed
+	glfwTerminate();
+	return 0;
+}
+
+// Communicate any window resizes to OpenGL
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+// Process inputs given to the window
+void processInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
